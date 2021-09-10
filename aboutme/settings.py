a95bc7f9eaa444ff4177e29ab5416bc4e9cd327c
vignxs <vignxs@gmail.com>
2021-09-10 12:43:53 +0500

    again static3 setting

--- aboutme/settings.py
+++ aboutme/settings.py
@@ -27,7 +27,7 @@ with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
     SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['about-vignxs.herokuapp.com']
 
