58ea41dad4a56841360c5ace0acd55f8a6c7a785
vignxs <vignxs@gmail.com>
2021-09-10 19:18:47 +0500

    dveh

--- aboutme/settings.py
+++ aboutme/settings.py
@@ -143,7 +143,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-# HTTPS settings
+ #HTTPS settings
 SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 SECURE_SSL_REDIRECT = True
