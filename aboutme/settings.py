d0960102553cde18beaff9d3cfb87157e9d5f8ba
vignxs <vignxs@gmail.com>
2021-09-10 12:31:21 +0500

    again static3 setting

--- aboutme/settings.py
+++ aboutme/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 STATIC_URL = '/staticfiles/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'staticfiles')
 ]
 
 # Simplified static file serving.
