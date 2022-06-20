a932b0651364927686a22548c90f266ad34d1845
vignxs <vignxs@gmail.com>
2022-06-20 15:26:12 -0500

    added changes

--- base/urls.py
+++ base/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('', views.home)
+    path('', views.home),
+    path("legacy/", views.legacy, name="legacy"),
+    path("version2/", views.version2, name="version2")
 ]
--- base/views.py
+++ base/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'base/home.html')
+
+def legacy(request):
+    return render(request, "legacy.html")
+  
+def version2(request):
+    return render(request, "version2.html")
