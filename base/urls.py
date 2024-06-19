e6223b609ad6bd6c1a9d19bada4201fb498bc87d
vignxs <vignxs@gmail.com>
2024-06-19 17:43:50 +0530

    updated code

--- base/urls.py
+++ base/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns =[
     path('', views.home),
     path("legacy/", views.legacy, name="legacy"),
-    path("version2/", views.version2, name="version2")
+    path("version2/", views.version2, name="version2"),
+    path('about/', views.about, name='about'),
+    path('projects/', views.projects, name='projects'),
+    path('contact/', views.contact, name='contact'),
 ]
--- base/views.py
+++ base/views.py
@@ -10,3 +10,14 @@ def legacy(request):
   
 def version2(request):
     return render(request, "version2.html")
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def projects(request):
+    return render(request, 'projects.html')
+
+
+def contact(request):
+    return render(request, 'contact.html')
